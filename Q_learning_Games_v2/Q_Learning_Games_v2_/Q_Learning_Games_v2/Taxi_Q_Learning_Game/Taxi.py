b43773838d433bd2ddfc91dba098b4111864c384
Giacomo <ferrogiacomo26@gmail.com>
2018-11-15 10:01:21 +0100

    Add files via upload

---
+++ Q_learning_Games_v2/Q_Learning_Games_v2_/Q_Learning_Games_v2/Taxi_Q_Learning_Game/Taxi.py
@@ -0,0 +1,21 @@
+import gym
+#import taxi env
+from gym.envs.toy_text import taxi
+#import tabular q agent
+import tabular_q_agent_taxi
+
+env=taxi.TaxiEnv()#make TaxiEnv
+
+agent=tabular_q_agent_taxi.TabularQAgentTaxi(env.observation_space,env.action_space)
+
+print("BEGIN THE Q-LEARNING")
+for i in range(100):
+	agent.learn(env) #learn best choices and act on the env
+	print("trial number: %d" %i)
+	if(i==0):
+		agent.LogUpdate()
+	if(i==50):
+		agent.LogUpdate()
+
+agent.LogUpdate()
+
---
+++ plots_azione_valore/print_state_plot.m
@@ -0,0 +1,11 @@
+clc;close all;clear all;
+
+x= [number of actions]; % sostituire a seconda dell'ambiente
+y=[values of each action]; % sostituire a seconda dell'ambiente
+
+figure;
+plot(x,y);
+
+title('title');
+xlabel('azioni');
+ylabel('valore');
